8d3e2e8fab51186de5408a930d725a28341e928e
박지수 <parkjisu6239@gmail.com>
2025-10-24 11:08:23 +0900

    React Strict Mode 비활성화

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false, // React Strict Mode 비활성화
 };
 
 export default nextConfig;
